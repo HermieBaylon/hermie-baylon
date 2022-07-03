e00abbe79ba1f902563dddf2eb3958fb115f444c
HermieBaylon <hbaylon@westmonroe.com>
2022-07-03 09:09:11 -0700

    Transfer to different Github

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,55 +1,56 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [navMenu, setNavMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
-    const[navMenu, setNavMenu] = useState(false)
-    const[screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const toggleMenu = () => {
+    setNavMenu(!navMenu);
+  };
 
-    const toggleMenu = () => {
-        setNavMenu(!navMenu)
-    }
-    
-    useEffect(() => {
-        const changeWidth = () => {
-            setScreenWidth(window.innerWidth);
-        }
-        window.addEventListener('resize', changeWidth)
-        return () => {
-            window.removeEventListener('resize', changeWidth)
-        }
-    }, [])
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", changeWidth);
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
 
-    return (
-        <nav class="header">
-        {(navMenu || screenWidth > 500) && (
-          <ul class="navbar">
-            <li className='item'>
-              <Link to="/">Home</Link>
-            </li>
-            <li className='item'>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li className='item'>
-              <Link to="/about">About</Link>
-            </li>
-            <li className='item'>
-              <Link to="/contact">Contact Me</Link>
-            </li>
-          </ul>
-        )}
-        {!navMenu && (
-          <ul class="navbar">
-            <li>
-            </li>
-          </ul>
-        )}
-          <GiHamburgerMenu onClick={toggleMenu} className='hamburger'></GiHamburgerMenu>
-      </nav>
-    )
+  return (
+    <nav className="header">
+      {(navMenu || screenWidth > 500) && (
+        <ul className="navbar">
+          <li className="item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="item">
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li className="item">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="item">
+            <Link to="/contact">Contact Me</Link>
+          </li>
+        </ul>
+      )}
+      {!navMenu && (
+        <ul className="navbar">
+          <li></li>
+        </ul>
+      )}
+      <GiHamburgerMenu
+        onClick={toggleMenu}
+        className="hamburger"
+      ></GiHamburgerMenu>
+    </nav>
+  );
 }
