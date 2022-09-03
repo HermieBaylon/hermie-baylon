de8fe0981a6598e22636eeb0514a34a7cbf446ef
Hermie Baylon <hermib100@gmail.com>
2022-09-02 18:40:52 -0700

    Home page responsive update

--- src/Data/DataIndustries.js
+++ src/Data/DataIndustries.js
@@ -1,32 +1,32 @@
 export default [
-    {
-        id: 1,
-        image: "software.jpg",
-        name: "High-Tech & Software"
-    },
-    {
-        id: 2,
-        image: "consultingManagement.jpg",
-        name: "Consulting Management"
-    },
-    {
-        id: 3, 
-        image: "education.jpg",
-        name: "Higher Educational Services"
-    },
-    {
-        id: 4, 
-        image: "sportsAndRecreation.jpg",
-        name: "Entertainment, Sports, & Recreation"
-    },
-    {
-        id: 5, 
-        image: "transportation.jpg",
-        name: "Transportation Services"
-    },
-    {
-        id: 6, 
-        image: "money.jpg",
-        name: "Financial & Banking Services"
-    }
-]
+  {
+    id: 1,
+    image: "software.jpg",
+    name: "Software",
+  },
+  {
+    id: 2,
+    image: "consultingManagement.jpg",
+    name: "Consulting",
+  },
+  {
+    id: 3,
+    image: "education.jpg",
+    name: "Education",
+  },
+  {
+    id: 4,
+    image: "sportsAndRecreation.jpg",
+    name: "Recreation",
+  },
+  {
+    id: 5,
+    image: "transportation.jpg",
+    name: "Transportation",
+  },
+  {
+    id: 6,
+    image: "money.jpg",
+    name: "Financial",
+  },
+];
